2b2bcbc6ff77bf09a5431cb05f1cb41aeae98c39
chasonghui <chasonghui@naver.com>
2021-03-25 17:50:05 +0900

    Add capture function

--- app/src/main/java/com/example/android_test/MainActivity.java
+++ app/src/main/java/com/example/android_test/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android_test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,10 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        just();
+        capture();
     }
     void just()
     {
+        Log.v("Check","여기들어오나 ?");
         System.out.println("그냥 만들어본 함수..");
         System.out.println("터미널에서 git 사용 되던가");
+    }
+    void capture()
+    {
+        //캡쳐 기능을 이렇게 뚝딱만들수 있다면 을매나 좋을꼬
+        System.out.println("캡쳐기능을 만들었다. 나는 유능한 팀원1.");
     }
 }
